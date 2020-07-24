e368016843cc629dc3d3fdc96b2d1a61a5564571
rbt gr <rr777robert@gmail.com>
2020-07-24 15:04:56 +0900

    ファクトリーパターン

--- shacho.rb
+++ shacho.rb
@@ -1,20 +1,29 @@
 # -*- cording: utf-8 -*-
 
-require_relative 'shain'
-require_relative 'tanto'
-require_relative 'shunin'
-require_relative 'bucho'
+# require_relative 'shain'
+# require_relative 'tanto'
+# require_relative 'shunin'
+# require_relative 'bucho'
 
-shain = Tanto.new
+require_relative 'shain_factory'
+
+shain_factory = ShainFactory.new
+
+#shain = Tanto.new
+
+shain = shain_factory.create('Tanto',100)
 puts shain.standup
 puts "私の給料は#{shain.calculate_salary(100)}円です"
 
-shain = Shunin.new
+shain = shain_factory.create('Shunin',100)
 puts shain.standup
 puts "私の給料は#{shain.calculate_salary(100)}円です"
 
-shain = Bucho.new
+shain = shain_factory.create('Bucho',100)
 puts shain.standup
 puts "私の給料は#{shain.calculate_salary(100)}円です"
 
+shain = shain_factory.create('Torishimariyaku',100)
+puts shain.standup
+puts "私の給料は#{shain.calculate_salary(100)}円です"
 
--- shain.rb
+++ shain.rb
@@ -16,3 +16,51 @@ class Shain
   #   base
   # end
 end
+
+
+class Tanto < Shain
+  def standup
+    puts '担当は慌てて起立しました'
+  end
+end
+
+class Shunin < Shain
+  def standup
+    '主任が素早くたち上がりました'
+  end
+
+  def base
+    @base * 2
+  end
+
+  def addition
+    1
+  end
+end
+
+class Bucho < Shain
+  def standup
+    '部長がだるそうに立ち上がりました'
+  end
+
+  def base
+    @base * 3
+  end
+end
+
+class Torishimariyaku < Shain
+  def standup
+    '踏ん反り返って立ち上がりました'
+
+  end
+
+  def base
+    @base * 4
+  end
+
+  def addition
+    2
+  end
+
+end
+
