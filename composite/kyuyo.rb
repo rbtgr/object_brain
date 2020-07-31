5af0473f223bca8dcf6675937e60172058d9b72c
rbt gr <rr777robert@gmail.com>
2020-08-01 01:27:37 +0900

    コンポジットパターン

---
+++ composite/kyuyo.rb
@@ -0,0 +1,11 @@
+class Kyuyo
+  attr_accessor :employee, ;meisai
+  attr_reader :kyuyo_bango
+
+  def initialize(kyuyo_bango)
+    @kyuyo_bango = kyuyo_bango
+  end
+
+end
+
+
---
+++ composite/unit.rb
@@ -0,0 +1,31 @@
+class Unit
+  attr_accessor :name, :contacts
+
+  def initialize(name, contacts)
+    @name, @contacts = name, contacts
+
+  end
+
+  def work
+    puts '働きます'
+  end
+
+  class Shain < Unit
+    def work
+      puts '社員は働いています'
+    end
+  end
+
+  class Bumon < Unit
+    attr_accessor :units
+
+    def initialize(name, contacts, units)
+      super
+      @units = units || []
+    end
+
+    def work
+      puts 'チームワークを発揮します'
+    end
+  end
+end
