10badc5f75293755f33f0751a5abe8091606c8e6
rbt gr <rr777robert@gmail.com>
2020-08-04 23:45:09 +0900

    職務のサブクラス化

--- rails/jinjiSystem/db/migrate/20200804135649_rename_column_yakusyoku_type.rb
+++ rails/jinjiSystem/db/migrate/20200804135649_rename_column_yakusyoku_type.rb
@@ -1,5 +1,5 @@
 class RenameColumnYakusyokuType < ActiveRecord::Migration[6.0]
   def change
-    rename_column :shains, :yakushoku, :type
+    rename_column :shains, :yakusyoku, :type
   end
 end
--- rails/jinjiSystem/db/schema.rb
+++ rails/jinjiSystem/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_134549) do
+ActiveRecord::Schema.define(version: 2020_08_04_135649) do
 
   create_table "shains", force: :cascade do |t|
     t.string "name"
-    t.string "yakusyoku"
+    t.string "type"
     t.integer "kihonkyu"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
